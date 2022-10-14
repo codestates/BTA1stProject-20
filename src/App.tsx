8f81849d63f75c57d0eebd8c6d806c1184e4c623
Sam Rho <samrho.dev@gmail.com>
2022-10-14 15:48:21 +0900

    ui: theme 추가 test(darkmode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useMemo, useState} from 'react';
+import {Box, createTheme, CssBaseline, IconButton, ThemeProvider} from "@mui/material";
+import {Brightness4 as Brightness4Icon, Brightness7 as Brightness7Icon} from '@mui/icons-material';
+import {ColorModeContext} from './contexts';
 
+// TODO: theme change
+// TODO: eslint, prettier 설정 추가
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [mode, setMode] = useState<'light' | 'dark'>('light');
+    const colorMode = useMemo(() => ({
+        toggleColorMode: () => {
+            setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        },
+    }), []);
+
+    const theme = useMemo(() =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode]);
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <CssBaseline />
+            <ThemeProvider theme={theme}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        width: '100%',
+                        height: '100%',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: 'background.default',
+                        color: 'text.primary',
+                        p: 3,
+                    }}
+                >
+                    {theme.palette.mode} mode
+                    <IconButton sx={{ml: 1}} onClick={colorMode.toggleColorMode} color="inherit">
+                        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                    </IconButton>
+                </Box>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
