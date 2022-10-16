41c08990d4692db85637b4b52e392691fc0083e1
joykim93 <joykim9311@gmail.com>
2022-10-16 15:25:45 +0900

    feat: sign up (create wallet) API

--- server/src/route/api.ts
+++ server/src/route/api.ts
@@ -4,10 +4,11 @@ const router = express.Router();
 import WalletController from "../controller/walletController";
 
      // Wallet (지갑)
-     router.get         ("/wallet/mnemonic"              ,WalletController.generateMnemonicCode     );
-     router.get         ("/wallet/address/:index"              ,WalletController.getWalletAddress     );
-     router.get         ("/wallet/balance"              ,WalletController.balanceOf     );
+     router.post         ("/wallet/create"              ,WalletController.createWallet     );
      router.post         ("/wallet/transfer"              ,WalletController.transfer     );
+     router.get         ("/wallet/mnemonic"              ,WalletController.generateMnemonicCode     );
+     router.get         ("/wallet/balance"              ,WalletController.balanceOf     );
+     
 
 
 export default router;
